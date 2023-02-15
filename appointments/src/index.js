37cc03c63b27a1d690c480827e4fe5fc486c046b
Sharonjep <jepngetichs65@gmail.com>
2023-02-15 16:07:50 +0300

    booking

--- appointments/src/index.js
+++ appointments/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import BookingModule from './components/BookingModule'
+import './components/style.css'
 
  
 const reactContentRoot=document.getElementById("root")
